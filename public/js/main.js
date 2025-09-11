365f17cadbbe0d0724a3531f91c15d28af9c6766
Andreas Keating <aplandolit@wpi.edu>
2025-09-10 22:28:30 -0400

    A2 requirements met. Complete

--- public/js/main.js
+++ public/js/main.js
@@ -17,8 +17,9 @@
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
       });
-      const j = await res.json();
+      //const j = await res.json();
 
+      await res.json();
       addForm.reset();
     });
   }
@@ -34,35 +35,36 @@
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ id })
       });
-      const j = await res.json();
+      //const j = await res.json();
 
+      await res.json();
       delForm.reset();
     });
   }
 })();
 
-const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
+// const submit = async function( event ) {
+//   // stop form submission from trying to load
+//   // a new .html page for displaying results...
+//   // this was the original browser behavior and still
+//   // remains to this day
+//   event.preventDefault()
   
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+//   const input = document.querySelector( "#yourname" ),
+//         json = { yourname: input.value },
+//         body = JSON.stringify( json )
 
-  const response = await fetch( "/submit", {
-    method:"POST",
-    body 
-  })
+//   const response = await fetch( "/submit", {
+//     method:"POST",
+//     body 
+//   })
 
-  const text = await response.text()
+//   const text = await response.text()
 
-  console.log( "text:", text )
-}
+//   console.log( "text:", text )
+// }
 
-window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
-}
+// window.onload = function() {
+//    const button = document.querySelector("button");
+//   button.onclick = submit;
+// }
